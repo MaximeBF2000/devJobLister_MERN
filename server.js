027a5503b18f03a85c5b45b20e87b7e44cb2211b
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-05-01 20:27:59 +0200

    heroku try

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ app.use("/api/jobs", jobRouter)
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"))
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
 }
 
 
